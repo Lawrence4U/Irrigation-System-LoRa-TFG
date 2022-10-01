a423c4a81912c14503dfce0c57b6041459aa8106
Lawrence4U <60020067+Lawrence4U@users.noreply.github.com>
2022-10-02 00:45:46 +0200

    idea WIP

--- GUI-Reworked.py
+++ GUI-Reworked.py
@@ -1,10 +1,15 @@
 from pickle import TRUE
-import tkinter as tk
+from time import sleep
+from tkinter import *
+from tkinter import ttk
 from xmlrpc.client import Boolean
 import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib.sankey import RIGHT
+
+
+
 
 def intVartoInt(lista):
     lista = [i.get() for i in lista]
@@ -19,19 +24,19 @@ def cButtonEvent():
     
 if __name__ == "__main__":
     events = ""
-    root = tk.Tk()
-    frame = tk.Frame(root)
+    root = Tk()
+    root.geometry("400x400+300+300")
+    frame = Frame(root)
     frame.pack()
-    opcion= [tk.IntVar(value=0),tk.IntVar(value=0),tk.IntVar(value=0),tk.IntVar(value=0),tk.IntVar(value=0),tk.IntVar(value=0),tk.IntVar(value=0)]
-    ch1 = tk.Checkbutton(root, text="L", variable=opcion[0], command=cButtonEvent, indicatoron=0)
+    opcion= [IntVar(value=0),IntVar(value=0),IntVar(value=0),IntVar(value=0),IntVar(value=0),IntVar(value=0),IntVar(value=0)]
+    ch1 = Checkbutton(frame, text="L", variable=opcion[0], command=cButtonEvent, indicatoron=0)
     ch1.pack()
-    ch2 = tk.Checkbutton(root, text="M", variable=opcion[1], command=cButtonEvent  ,indicatoron=0)
+    ch2 = Checkbutton(frame, text="M", variable=opcion[1], command=cButtonEvent  ,indicatoron=0)
     ch2.pack()
-    ch3 = tk.Checkbutton(root, text="X", variable=opcion[2], command=cButtonEvent ,indicatoron=0)
+    imagen = PhotoImage(file="imagenes\circulo.png").subsample(40,40)
+    imagen2 = PhotoImage(file="imagenes\circulo2.png").subsample(40,40)
+    ch3 = Checkbutton(frame, text="X", variable=opcion[2], command=cButtonEvent , indicatoron=0, image=imagen, selectimage=imagen2)
     ch3.pack()
-    monitor = tk.Label(root)
-    monitor.pack()
     # tk.Radiobutton(frame,text="M", indicatoron=0)
     # lanzar hilos para el resto de la app
     root.mainloop()
-    print("Hola")

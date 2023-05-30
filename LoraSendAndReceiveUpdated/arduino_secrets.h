46c93517c57edcffda7248a840adcb07b4585119
Alvaro <allorenzocastro@gmail.com>
2023-05-30 19:22:50 +0200

    Config del GW introducida

--- LoraSendAndReceiveUpdated/arduino_secrets.h
+++ LoraSendAndReceiveUpdated/arduino_secrets.h
@@ -1,3 +1,3 @@
 // Replace with keys obtained from TheThingsNetwork console
 #define SECRET_APP_EUI "0000000000000000"
-#define SECRET_APP_KEY "0708CDD83E2D375D7ACE1D7F9C82AF20"
+#define SECRET_APP_KEY "d831c7d163c0f1626b68d93459171f9a"
